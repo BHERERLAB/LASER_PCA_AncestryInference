1ef9dd0de27e8ad395b15ac06858fd2448171d6d
Justin Pelletier <62766152+JustinPelletier@users.noreply.github.com>
2025-10-15 10:38:25 -0400

    Update launch_da.sh

--- launch_da.sh
+++ launch_da.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#SBATCH --account=ctb-hussinju
-#SBATCH --time=48:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
-#SBATCH --job-name=laser_pca
-#SBATCH --output=parallel_launch_nextflow_PCA.out
-#SBATCH --error=parallel_launch_nextflow_PCA.err
+#SBATCH --account=<user_account>
+#SBATCH --time=<12:00:00>
+#SBATCH --cpus-per-task=<1>
+#SBATCH --mem=<16G>
+#SBATCH --job-name=<laser_pca>
+#SBATCH --output=launch_da.out
+#SBATCH --error=launch_da.err
 
 
 module load nextflow
 
 
-nextflow run parallel_PCA_on_HGDP1KG.nf -c parallel_nextflow.config -resume
-
+nextflow run main.nf -c nextflow.config -resume
 
 
 echo "DONE"
